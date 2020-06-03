663340bfa9f2d9c792e62f980161275cefde3b1a
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-03 09:40:45 -0400

    Cahnges to More Numeric Values

--- MoreNumericTypes/Program.cs
+++ MoreNumericTypes/Program.cs
@@ -6,7 +6,14 @@ namespace MoreNumericTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Number of Miles Driven?");
+            int mile = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Number of Gallons Consumed?");
+            int gallons = Int32.Parse(Console.ReadLine());
+
+            int mpg = mile / gallons;
+            Console.WriteLine($"Your MPG for the trip was: {mpg}");
         }
     }
 }
