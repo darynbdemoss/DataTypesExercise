66140d7687a5d0ab6f6c26641789c570e311213b
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-03 09:30:56 -0400

    Changes to Numeric Values

--- NumericValues/Program.cs
+++ NumericValues/Program.cs
@@ -6,7 +6,16 @@ namespace NumericValues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the area of rectangle program");
+            Console.WriteLine("Length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Width: ");
+            int width = Int32.Parse(Console.ReadLine());
+
+            int area = length * width;
+
+            Console.WriteLine($"The area of the rectangle is : {area}");
         }
     }
 }
