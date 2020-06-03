e2b27f952c2dd4136f3480237f31678aa197a689
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-03 09:16:25 -0400

    Add project files.

---
+++ DataTypesExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataTypesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
